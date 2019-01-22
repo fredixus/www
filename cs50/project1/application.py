4218bee9b1a5c9e8413ba9a39ad3179ab176650a
Michał Dębosz <michadbsz@gmail.com>
2019-01-22 16:04:26 +0100

    updated files, added new feature

--- cs50/project1/application.py
+++ cs50/project1/application.py
@@ -1,7 +1,7 @@
 import os
 import requests
 
-from flask import Flask, session
+from flask import Flask, session, render_template, request
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
@@ -23,7 +23,29 @@ db = scoped_session(sessionmaker(bind=engine))
 
 res = requests.get("https://www.goodreads.com/book/review_counts.json", params={"key": "uFHO4yqNDsoifSLOVF07bA", "isbns": "9781632168146"})
 
+"""
+@app.route("/<string:name>")
+def hello(name):
+    name = name.capitalize()
+    return f"<h1>Hello, {name}!</h1>"
+"""  
+  
+aNames = ["Adam","Michal","Pablo"]
+aLinks = ["register","Michal","index"]
 @app.route("/")
+@app.route("/index")
 def index():
-    return "Project 1: TODO - with changes <br>"+ str(res.json())
+    headline = "Hello, world!"
+    return render_template("index.html", title="Strona Glowna",headline=headline, new_year=True,names = aNames,links = aLinks)
+    #return "Project 1: TODO - with changes <br>"+ str(res.json())
 
+@app.route("/register")
+def register():
+    return render_template("register.html", title="Rejestracja",links = aLinks,headline="Rejestracja")
+
+
+@app.route("/hello", methods=["GET"])
+def hello():
+    name = request.form.get("name") # take the request the user made, access the form,
+                                    # and store the field called `name` in a Python variable also called `name`
+    return render_template("hello.html",name=name)
