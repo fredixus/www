47bc867f6c5647d6bd31d68fe88dd4cbf8fad419
Michał Dębosz <michadbsz@gmail.com>
2019-01-28 16:06:10 +0100

    Add files via upload

--- cs50/project1/application.py
+++ cs50/project1/application.py
@@ -1,4 +1,4 @@
-import os
+﻿import os
 import requests
 
 from flask import Flask, session, render_template, request
@@ -34,8 +34,20 @@ def hello(name):
     return f"<h1>Hello, {name}!</h1>"
 """
 
-aNames = ["Adam","Michal","Pablo"]
-aLinks = ["register","Michal","index","notes"]
+aNames = [
+"Registration: Users should be able to register for your website, providing (at minimum) a username and password.",
+"Login: Users, once registered, should be able to log in to your website with their username and password.",
+"Logout: Logged in users should be able to log out of the site.",
+"Import: Provided for you in this project is a file called books.csv, which is a spreadsheet in CSV format of 5000 different books. Each one has an ISBN number, a title, an author, and a publication year. In a Python file called import.py separate from your web application, write a program that will take the books and import them into your PostgreSQL database. You will first need to decide what table(s) to create, what columns those tables should have, and how they should relate to one another. Run this program by running python3 import.py to import the books into your database, and submit this program with the rest of your project code.",
+"Search: Once a user has logged in, they should be taken to a page where they can search for a book. Users should be able to type in the ISBN number of a book, the title of a book, or the author of a book. After performing the search, your website should display a list of possible matching results, or some sort of message if there were no matches. If the user typed in only part of a title, ISBN, or author name, your search page should find matches for those as well!",
+"Book Page: When users click on a book from the results of the search page, they should be taken to a book page, with details about the book: its title, author, publication year, ISBN number, and any reviews that users have left for the book on your website.",
+"Review Submission: On the book page, users should be able to submit a review: consisting of a rating on a scale of 1 to 5, as well as a text component to the review where the user can write their opinion about a book. Users should not be able to submit multiple reviews for the same book.",
+"Goodreads Review Data: On your book page, you should also display (if available) the average rating and number of ratings the work has received from Goodreads.",
+"API Access: If users make a GET request to your website’s /api/<isbn> route, where <isbn> is an ISBN number, your website should return a JSON response containing the book’s title, author, publication date, ISBN number, review count, and average score. The resulting JSON should follow the format"
+]
+aLinks = ["index","login","register"]
+aLinksAfetrLogin = ["index","search","logout"]
+
 @app.route("/")
 @app.route("/index")
 def index():
@@ -64,6 +76,7 @@ def notes():
     return render_template("notes.html", notes=session["notes"])
 
 #afterReg
+
 @app.route("/afterReg", methods=["GET", "POST"])
 def afterReg():
     
@@ -77,15 +90,38 @@ def afterReg():
         name = request.form.get("name")
         passwd = request.form.get("pass")
         session["register"].append({"Id":id,"Name: ":name,"Pass: ":passwd})
-    return render_template("afterReg.html", header = "Database snapshot",users=session["register"])
+    return render_template("afterReg.html", header = "Database snapshot",users=session["register"][-1]['Name: '])
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
+    ValueOf = False
+    tmp = ""
     if session.get("login") is None:
-        session["login"] = []
-    if request.method == "POST":  
+        session["login"]= []
+    if request.method == "POST":
         name = request.form.get("name")
         passwd = request.form.get("pass")
-        session["login"].append({"Name: ":name,"Pass: ":passwd})
+        #session["login"].append({"Id":id,"Name: ":name,"Pass: ":passwd})
+        
+        """if name in session["register"][:][1] and passwd in session["register"][:][2]:
+            ValueOf = True
+            tmp = str(name in session["register"][:][1])
+            
+        else:
+            ValueOf = False
+        tmp = session["register"]    
+            """      
     return render_template("login.html", users=session["login"], title="Logowanie",links = aLinks,headline="Logowanie")
+
+@app.route("/users")
+def users():
+    return render_template("users.html", header = "Users",links = aLinks,users=session["register"])
+
+@app.route("/search")
+def search():
+    return render_template("search.html", header = "Szukaj",links = aLinksAfetrLogin)
+    
+@app.route("/afterSearch")
+def afterSearch():
+    return render_template("afterSearch.html", header = "afterSearch",links = aLinksAfetrLogin)
     
